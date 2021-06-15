7b1a81f77852872be672e0b301a07c3d086a8ee0
yijun <sandy3068@yahoo.com.tw>
2021-06-15 19:19:09 +0800

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <div class="card-body">This is some text within a card body.</div>
+    {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: '我是內容',
+    };
+  },
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{name}}
+    <Card></Card>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card.vue';
+
+export default {
+  data() {
+    return {
+      name: process.env.VUE_APP_NAME,
+    };
+  },
+  components: {
+    Card,
+  },
+};
+</script>
